6179be1ef04f0ecf99b88022b190bd588000c067
Cooper Lewis <thatcooperlewis@gmail.com>
2018-10-03 12:05:15 -0400

    more comments

--- config.py
+++ config.py
@@ -1,8 +1,9 @@
 
 # CONFIG OPTIONS
-
 # the string being replaced in file
 placeholder = 'VARIABLE'
+# choose to ignore (harmless) MacOS warning of duplicates.
+# this doesn't actually do anything yet
 ignore_macos_warnings = True
 
 
