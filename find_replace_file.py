341e611807cd0b0721e5c871f74ac7d6916dae62
Cooper Lewis <thatcooperlewis@gmail.com>
2018-10-03 21:35:04 -0400

    error message/about for empty file

--- find_replace_file.py
+++ find_replace_file.py
@@ -1,18 +1,29 @@
 from tkinter import filedialog as filer
 from tkinter import *
+import tkinter.messagebox
 import pyperclip
+import sys
 
 
 def prompt_name():
-    var = input("Desired class name:").strip()
+    var = input("Desired class/variable name:").strip()
     return var
 
 
 def prompt_file():
+
     # request file location
     file_import = Tk()
     file_import.withdraw()
     file_import.filename = filer.askopenfilename(initialdir="/data", title="Select template\n")
+
+    # checks for empty file
+    try:
+        open(file_import.filename, 'r')
+    except (FileNotFoundError):
+        tkinter.messagebox.showinfo("Error", "File is empty/No file selected")
+        sys.exit()
+
     # write file to string
     with open(file_import.filename, 'r') as t:
         template = t.read().strip()
